09d30e52a40739bc2bc0cf1387f582df2f14e8e9
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-03-23 16:26:14 -0400

    Create NotebookVersion.py

---
+++ NotebookVersion.py
@@ -0,0 +1,22 @@
+LastestStableBuild = 15.025
+NotebookRange = 2.0
+
+def versionProcess(rVersion):
+  versionActual = float(rVersion)
+  versionChecker = LastestStableBuild
+  forceUpdate = versionChecker - versionActual
+    
+  if versionActual == versionChecker:
+      print("You are running the latest version - Build",versionChecker)
+  elif forceUpdate >= NotebookRange:
+      ErrorCode = "File Exited, Error 114x0001 - File Too Outdated, Update Notebook"
+      sys.exit(ErrorCode)
+  elif versionActual > versionChecker:
+      ErrorCode = "File Exited, Error 114x0002 - Version Newer?"
+      sys.exit(ErrorCode)
+  elif versionActual < versionChecker:
+      print("You are running an outdated version of this notebook!")
+      print("Running behind", forceUpdate, "version(s). Update soon!")
+  else:
+      ErrorCode = "File Exited, Failed To Check For Update"
+      sys.exit(ErrorCode)
