fb5312dbc53ce069b70a19bef6809d6f88226962
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-03-24 17:04:19 -0400

    Update VersionNotebook.py

--- VersionNotebook.py
+++ VersionNotebook.py
@@ -2,6 +2,26 @@ import sys
 LastestStableBuild = 15.35
 NotebookRange = 2.0
 
+def preVersionProcess(rVersion):
+  versionActual = float(rVersion)
+  versionChecker = LastestStableBuild
+  forceUpdate = versionChecker - versionActual
+  forceUpdate = float("{:.2f}".format(forceUpdate))
+    
+  if versionActual == versionChecker:
+      print("You are running the latest version - Build", versionChecker)
+  elif forceUpdate >= NotebookRange:
+      print("File Will Exit, Error 114x0001 - File Too Outdated, Update Notebook\n**In order to bypass update checker, if you choose, set BypassVersionUpdateChecker to True in the Mainframe section.**")
+  elif versionActual > versionChecker:
+      ErrorCode = "File Exited, Error 114x0002 - Version Newer?"
+      sys.exit(ErrorCode)
+  elif versionActual < versionChecker:
+      print("You are running an outdated version of this notebook!")
+      print("Running behind", forceUpdate, "version(s). Update soon!")
+  else:
+      ErrorCode = "File Exited, Failed To Check For Update"
+      sys.exit(ErrorCode)
+
 def versionProcess(rVersion):
   versionActual = float(rVersion)
   versionChecker = LastestStableBuild
