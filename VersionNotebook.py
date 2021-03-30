bd624d4e6a54db7b528f2afdab4693a31e724914
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-03-30 16:39:32 -0400

    Update VersionNotebook.py

--- VersionNotebook.py
+++ VersionNotebook.py
@@ -28,25 +28,25 @@ def vBuildVersion(rVersion):
   
 
 def preVersionProcess(rVersion):
-  pip install termcolor
-  from termcolor import colored
   versionActual = float(rVersion)
   versionChecker = LatestStableBuild
   forceUpdate = versionChecker - versionActual
   forceUpdate = float("{:.2f}".format(forceUpdate))
+  
+  ENDC = '\033[m' # reset to the defaults
   
   if versionActual == 1.337:
       print("You are running a developer build, good luck with the coding.")
   elif versionActual == versionChecker:
       print("You are running the latest version - Build", versionChecker)
   elif forceUpdate >= NotebookRange:
-      print(color('File Will Exit, Error 114x0001 - File Too Outdated, Update Notebook\n**In order to bypass update checker, if you choose, set BypassVersionUpdateChecker to True in the Mainframe section.**', 'red'))
+      print(TGREEN + "File Will Exit, Error 114x0001 - File Too Outdated, Update Notebook\n**In order to bypass update checker, if you choose, set BypassVersionUpdateChecker to True in the Mainframe section.**", ENDC
   elif versionActual > versionChecker:
       ErrorCode = "File Exited, Error 114x0002 - Version Newer?"
       sys.exit(ErrorCode)
   elif versionActual < versionChecker:
       print("You are running an outdated version of this notebook!")
-      print("Running behind", forceUpdate, "version(s). Update soon!")
+      print(TGREEN + "Running behind", forceUpdate, "version(s). Update soon!", ENDC)
   else:
       ErrorCode = "File Exited, Failed To Check For Update"
       sys.exit(ErrorCode)
